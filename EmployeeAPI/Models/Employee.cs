2fa3863b986c29e264bfc376a207e3b0eab8f94a
jonataspm <jonataspmpm@gmail.com>
2023-03-11 18:15:50 -0300

    COMMIT - Alteração na entidade para filtrar possíveis erros

--- EmployeeAPI/Models/Employee.cs
+++ EmployeeAPI/Models/Employee.cs
@@ -1,12 +1,19 @@
-﻿namespace EmployeeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeAPI.Models
 {
     public class Employee
     {
         public  int Id { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         public string Name { get; set; }
 
+        [EmailAddress (ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Range(0, 1, ErrorMessage = "Inform if it is Active (1) ou Not Active (0)")]
         public int IsActive { get; set; }
     }
 }
